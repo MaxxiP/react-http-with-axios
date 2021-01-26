da7f1046838a3c0d0109e4de16d4f18df58d8fdf
MaxxiP <s0546567@htw-berlin.de>
2021-01-26 21:21:06 +0100

    Changed some route to use nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 //import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
+//import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
@@ -16,7 +16,7 @@ class Blog extends Component {
                         <ul>
                             <li><NavLink 
                                 exact 
-                                to="/" >Home</NavLink></li>
+                                to="/posts" >Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -26,9 +26,8 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/" exact component={Posts} />
-                    <Route path="/post/:id" exact component={FullPost} />
                     <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
 
             </div>
